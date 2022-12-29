2456316cd201be7e31203f86e5b6242a638d4874
ZHOU <1136025373@qq.com>
2022-12-29 22:36:02 +0800

    与springboot整合，增加security相关配置

--- service/service_zhou_activiti/src/main/java/com/nanjing/ActApplication.java
+++ service/service_zhou_activiti/src/main/java/com/nanjing/ActApplication.java
@@ -9,10 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description:
  **/
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
-})
+@SpringBootApplication
 public class ActApplication {
     public static void main(String[] args) {
         SpringApplication.run(ActApplication.class,args);
